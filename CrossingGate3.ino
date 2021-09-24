a7b8c602097126ff8895610c4011ae32dd19eb54
hefy-jefy <geoffrey.shipton@sbcglobal.net>
2021-09-23 18:10:23 -0700

    Updated version
    Extra sensor now handles long and short trains

--- CrossingGate3.ino
+++ CrossingGate3.ino
@@ -15,6 +15,9 @@
 //the stepper was running!
 //8/22/21 Tidied up, removed old flasher code.
 //8/28/21 Final tweaking of the closed / open values to match mechanics
+//9/5/21 Attempting to handle short trains! TODO: figure out how to open the gates!
+//9/23/21 Added a third sensor positioned at the crossing.  Code now handles short trains and long trains, also warning light
+//behaviour is more realistic.  
 
 
 #include <AccelStepper.h>
@@ -27,20 +30,32 @@
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
 AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
 
-int dt = 250;
+int dt = 100;
 int detect;
 int ambient;
 int dark = 625;
-int lightVal;
-int lightPin = A1;
+int lightVal1;
+int lightPin1 = A1;
 int lightVal2;
 int lightPin2 = A2;
+int lightVal3;
+int lightPin3 = A3; //Sensor at the crossing
 int ambientPin = A0;
 int gateOpen = 0; //   Rotates the stepper to the open posn.
-int gateClose = 3400; // Rotates the stepper to the closed (down) posn.
-int gateState;
+int gateClose = 3600; // Rotates the stepper to the closed (down) posn.
+int gateState = 0;
+int sens1 = 0;
+int sens1Old = 1;
+int sens1New;
+int sens2 = 0;
+int sens2Old = 1;
+int sens2New;
+int sens3 = 0;
+int sens3Old = 0;
+int sens3New = 0;
 int warningLeds = 4;
 int hallPin = 3; //Pin attached to Hall sensor, normally high.
+int dtt = 10000;  // delay to allow the train to clear the down sensor after opening the gate
 
 void homefunction() { //Resets the gate position to the closed position.
   stepper.setMaxSpeed(1000);
@@ -50,17 +65,16 @@ void homefunction() { //Resets the gate position to the closed position.
     stepper.runSpeed();
   }
   stepper.setCurrentPosition(0);  //Sets the motor position to 0 (the open position)
-
 }
-
 
 void setup() {
   homefunction();
   stepper.setMaxSpeed(1000); // Set the maximum steps per second:
   stepper.setSpeed(500);
   stepper.setAcceleration(400);  // Set the maximum acceleration in steps per second^2:
-  pinMode (lightPin, INPUT);
+  pinMode (lightPin1, INPUT);
   pinMode (lightPin2, INPUT);
+  pinMode (lightPin3, INPUT);
   pinMode (ambientPin, INPUT);
   pinMode (warningLeds, OUTPUT);
   Serial.begin(9600);
@@ -68,42 +82,72 @@ void setup() {
 
 void loop() {
 
-  ambient = analogRead(ambientPin); //Read ambient light from third photoresistor.
-  //Serial.println(ambient);
+  ambient = analogRead(ambientPin); //Read ambient light from ambient LDR.
+  //Serial.print(" ambient:");
+  //Serial.print(ambient);
   detect = ((ambient) + 150); //Set the detect value based on the ambient light
-  //Serial.println(detect);
-  lightVal = analogRead(lightPin); //           Read the #1 light detector (no train is <detect train is >detect
-  //Serial.println(lightVal);
-  lightVal2 = analogRead(lightPin2); //         Read the #2 light detector (no train is <detect train is >detect
-  //Serial.println(lightVal2);
+  //Serial.print(" detect:");
+  //Serial.print(detect);
+  lightVal1 = analogRead(lightPin1); //           Read the #1 LDR (no train is <detect train is >detect
+  //Serial.print(" lightVal1:");
+  //Serial.print(lightVal1);
+  lightVal2 = analogRead(lightPin2); //         Read the #2 LDR (no train is <detect train is >detect
+  //Serial.print(" lightVal2:");
+  //Serial.print(lightVal2);
+  lightVal3 = analogRead(lightPin3); //        Read the #3 LDR (no train is <detect train is >detect
+  //Serial.print(" lightVal3:");
+  //Serial.println(lightVal3);
   if (ambient < dark) { //      Check if ambient is too dark, skip to the end
-    if (lightVal > detect || lightVal2 > detect) { // Warning lights on if either sensor detects a train
-      digitalWrite (warningLeds, HIGH);
+
+    //**************************************************************************************
+    if (lightVal1 > detect) {              //If sensor 1 detects a train (distant sensor one direction)
+      sens1 = 1;
     }
-    if (lightVal < detect && lightVal2 < detect) { //Warning lights off if neither sensor detects a train
-      digitalWrite (warningLeds, LOW);
+    if (lightVal1 < detect) {
+      sens1 = 0;
     }
-    if (lightVal > detect && gateState == 0) { //   If there is a train and the gate is open (gateState=0)
+    //Serial.print(" sens 1:");
+    //Serial.print(sens1);
+    if (lightVal2 > detect) {             //If sensor 2 detects a train  (distant sensor other direction)
+      sens2 = 1;
+    }
+    if (lightVal2 < detect) {
+      sens2 = 0;
+    }
+    //Serial.print(" sens 2:");
+    //Serial.print(sens2);
+    if (lightVal3 > detect) {             //If sensor 3 detects a train (sensor 3 is at the crossing)
+      sens3 = 1;
+    }
+    if (lightVal3 < detect) {
+      sens3 = 0;
+    }
+    //Serial.print(" sens 3:");
+    //Serial.println(sens3);
+    //delay(500);
+    if (sens1 == 1 && gateState == 0) {
+      digitalWrite (warningLeds, HIGH);// Warning lights on gate closing
       stepper.moveTo(gateClose);
-      stepper.runToPosition();//             Close the gate to traffic.
-      gateState = 1; //                           Set gate State=1 (closed)
+      stepper.runToPosition();            //Close the gate to traffic.
+      gateState = 1;                      //Set gate State=1 (closed)
     }
-    if (lightVal < detect && gateState == 1 && lightVal2 < detect) { //If neither sensor detects a train and the gate is closed, open it.
-      stepper.moveTo(gateOpen);
-      stepper.runToPosition();//   Open the gate to traffic
-      gateState = 0; //                          Set gate State=0 (open)
-    }
-    if (lightVal2 > detect && gateState == 0) { // If there is a train (other direction) and the gate is open (gateState=0)
+    if (sens2 == 1 && gateState == 0) {
+      digitalWrite (warningLeds, HIGH);// Warning lights on gate closing
       stepper.moveTo(gateClose);
-      stepper.runToPosition();//             Close the gate to traffic.
-      gateState = 1; //                           Set gate State=1 (closed)
+      stepper.runToPosition();            //Close the gate to traffic.
+      gateState = 1;                      //Set gate State=1 (closed)
     }
-    if (lightVal2 < detect && gateState == 1 && lightVal < detect) { //If neither sensor detects a train and the gate is closed, open it.
-      stepper.moveTo(gateOpen);
-      stepper.runToPosition();//   Open the gate to traffic
-      gateState = 0; //                          Set gate State=0 (open)
-      delay(1000);
+    sens3New = sens3;
+    if (sens3Old == 1 && sens3New == 0) {
+      if (gateState == 1) {                //If the gate is closed, wait until the last car has cleared the crossing
+        stepper.moveTo(gateOpen);
+        stepper.runToPosition();           //Open the gate to road traffic.
+        gateState = 0;                    //Set gate State=0 (open)
+        digitalWrite (warningLeds, LOW); //Warning lights off, gate is open
+        delay(dtt);                      //delay to allow the train to clear the downstream sensor
+      }
     }
+    sens3Old = sens3New;
   }
-  stepper.disableOutputs();//Remove power from the stepper to avoid overheating
+  stepper.disableOutputs();               //Remove power from the stepper to avoid overheating
 }
