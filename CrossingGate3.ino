1903a24f36f8d4d012e748bd32ad0214ff419b61
hefy-jefy <geoffrey.shipton@sbcglobal.net>
2021-10-02 16:50:57 -0700

    Update CrossingGate3.ino

--- CrossingGate3.ino
+++ CrossingGate3.ino
@@ -10,15 +10,16 @@
 //different levels, might need to apply an offset.
 //8/16/21 moved to the Accelstepper driver
 //Made warninglights a subroutine
-//8/18/21Added a Hall sensor to reset the gate position at power-up runs in a sub-routine.
+//8/18/21Added a Hall sensor to reset the gate position at power-up. Runs in a sub-routine.
 //Warning LEDs replaced with external circuit. This avoids the problem of making the warninglights flash while
-//the stepper was running!
+//the stepper is running!
 //8/22/21 Tidied up, removed old flasher code.
 //8/28/21 Final tweaking of the closed / open values to match mechanics
 //9/5/21 Attempting to handle short trains! TODO: figure out how to open the gates!
 //9/23/21 Added a third sensor positioned at the crossing.  Code now handles short trains and long trains, also warning light
 //behaviour is more realistic.
 //9/29/21 Added an adjustment to sens3 for correct operation of gate-open function.
+//10/2/21 Added a smoothing routine to the #3 sensor.  This sensor is sensitive to the gaps between the cars, causing the the gate to open prematurly
 
 
 #include <AccelStepper.h>
@@ -39,27 +40,28 @@ int lightVal1;
 int lightPin1 = A1;
 int lightVal2;
 int lightPin2 = A2;
-int lightVal3;
 int lightPin3 = A3; //Sensor at the crossing
 int ambientPin = A0;
-int gateOpen = 0; //   Rotates the stepper to the open posn.
-int gateClose = 3600; // Rotates the stepper to the closed (down) posn.
+int gateOpen = 0;             //Rotates the stepper to the open posn.
+int gateClose = 3600;         //Rotates the stepper to the closed (down) posn.
 int gateState = 0;
 int sens1 = 0;
-int sens1Old = 1;
-int sens1New;
 int sens2 = 0;
-int sens2Old = 1;
-int sens2New;
 int sens3 = 0;
 int sens3Old = 0;
 int sens3New = 0;
-int warningLeds = 4; //Pin attached to the warning lights flasher circuit. 
-int hallPin = 3; //Pin attached to Hall sensor, normally high.
-int dtt = 10000;  // delay to allow the train to clear the down sensor after opening the gate
-int adj3sens = -40; // Fine adjuistment to Sens3 to get correct operation of the open-gate sens 3. 
+const int numReadings =  30; //amount of smoothing for #3 sensor
+int readings[numReadings];   // the readings from the analog input for #3 sensor
+int readIndex = 0;           // the index of the current reading
+float lightVal3total = 0;    // the running total of readings for #3 sensor
+int lightVal3;               // the smoothed value of the #3 sensor
 
-void homefunction() { //Resets the gate position to the open position, in case something went wrong.
+int warningLeds = 4;         //Pin attached to the warning lights flasher circuit.
+int hallPin = 3;             //Pin attached to Hall sensor, normally high.
+int dtt = 10000;             // delay to allow the train to clear the down sensor after opening the gate
+
+
+void homefunction() {        //Resets the gate position to the open position, in case something went wrong.
   stepper.setMaxSpeed(1000);
   stepper.setSpeed(500);
   pinMode(hallPin, INPUT);
@@ -72,7 +74,7 @@ void homefunction() { //Resets the gate position to the open position, in case s
 void setup() {
   homefunction();
   stepper.setMaxSpeed(1000); // Set the maximum steps per second:
-  stepper.setSpeed(500); 
+  stepper.setSpeed(500);
   stepper.setAcceleration(400);  // Set the maximum acceleration in steps per second^2:
   pinMode (lightPin1, INPUT);
   pinMode (lightPin2, INPUT);
@@ -80,26 +82,41 @@ void setup() {
   pinMode (ambientPin, INPUT);
   pinMode (warningLeds, OUTPUT);
   Serial.begin(9600);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;                             //Initialize all smoothing readings for Sensor#3 to zero
+  }
 }
 
 void loop() {
 
-  ambient = analogRead(ambientPin); //Read ambient light from ambient LDR.
+  ambient = analogRead(ambientPin);                        //Read ambient light from ambient LDR.
   Serial.print(" ambient:");
   Serial.print(ambient);
-  detect = ((ambient) + 150); //Set the detect value based on the ambient light
+  detect = ((ambient) + 150);                              //Set the detect value based on the ambient light
   Serial.print(" detect:");
   Serial.print(detect);
-  lightVal1 = analogRead(lightPin1); //           Read the #1 LDR (no train is <detect train is >detect
+  lightVal1 = analogRead(lightPin1);                       //Read the #1 LDR (no train is <detect train is >detect
   Serial.print(" lightVal1:");
   Serial.print(lightVal1);
-  lightVal2 = analogRead(lightPin2); //         Read the #2 LDR (no train is <detect train is >detect
+  lightVal2 = analogRead(lightPin2);                       //Read the #2 LDR (no train is <detect train is >detect
   Serial.print(" lightVal2:");
   Serial.print(lightVal2);
-  lightVal3 = analogRead(lightPin3)+(adj3sens); //        Read the #3 LDR (no train is <detect train is >detect.  Also apply fine adjust value.
+
+  //smoothing routine applied to sensor#3 to avoid gates opening prematurly due to gaps between cars.
+  lightVal3total = lightVal3total - readings[readIndex];  //Subtract the last reading
+  readings[readIndex] = analogRead(lightPin3);            //Read the #3 LDR
+  lightVal3total = lightVal3total + readings[readIndex];  //Add the reading to the total
+  readIndex = readIndex + 1;                              //Advance to the next position in the array
+  if (readIndex >= numReadings) {                         //If we are at the end of the array...
+    readIndex = 0;                                        //...Wrap around to the beginning.
+  }
+  lightVal3 = lightVal3total / numReadings;              //Take the average to get smoothed sensor #3 value
+  lightVal3 = lightVal3 - 40;                            //small correction to sensor #3
+
+  //lightVal3 = analogRead(lightPin3)+(adj3sens);
   Serial.print(" lightVal3:");
   Serial.println(lightVal3);
-  if (ambient < dark) { //      Check if ambient is too dark, skip to the end
+  if (ambient < dark) {                                  //Check if ambient is too dark, skip to the end
 
     //**************************************************************************************
     if (lightVal1 > detect) {              //If sensor 1 detects a train (distant sensor one direction)
